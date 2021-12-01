dc0d3bb1b627f1ce2e4b834a18ed7fe45bd2e24d
AlexGabor <alex.dan.gabor@gmail.com>
2021-12-01 08:26:20 +0200

    Add Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+fun part1(input: List<Int>): Int {
+    return (1 until input.size).count { index ->
+        input[index] > input[index - 1]
     }
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
+fun part2(input: List<Int>): Int {
+    return (1 until input.size - 2).count { index ->
+        input[index] + input[index + 1] + input[index + 2] > input[index - 1] + input[index] + input[index + 1]
     }
+}
+
+fun main() {
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
